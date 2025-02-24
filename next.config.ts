44295749cfa2638b855e3051bed93190f4df8a53
vinit2803 <vinitpatel2803@gmail.com>
2025-02-24 19:14:39 -0400

    fix github pages for deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/portfolio",
   assetPrefix: "/portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
