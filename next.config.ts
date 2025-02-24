219465469d303799e8720145d83bb502412ffad8
vinit2803 <vinitpatel2803@gmail.com>
2025-02-24 19:06:02 -0400

    setup githubactions fro deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
