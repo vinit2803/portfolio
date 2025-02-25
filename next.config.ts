a8b240b49265e05bb22b283a71c8219cc7ee9207
vinit2803 <vinitpatel2803@gmail.com>
2025-02-25 18:26:27 -0400

    changesss

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,5 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/portfolio", // Set base path for GitHub Pages
   assetPrefix: "/portfolio/", // Fix asset paths for CSS & JS
-
-  images: {
-    unoptimized: true, // Fix images in static export
-  },
 };
-
 export default nextConfig;
