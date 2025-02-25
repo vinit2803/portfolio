9ee825105417d95c8a0d19978b92eb7b91e6d034
vinit2803 <vinitpatel2803@gmail.com>
2025-02-25 18:02:31 -0400

    few changes for gitpages

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-
   images: {
     unoptimized: true, // Fix image paths in static export
   },
